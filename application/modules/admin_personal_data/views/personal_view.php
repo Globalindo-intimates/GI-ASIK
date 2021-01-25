f5d4e0ee2dfd8561ea0293fb5315217dc19c406b
diananggit <diananggi@globalindointimates.com>
2021-01-25 13:51:51 +0700

    add tombol

--- application/modules/admin_personal_data/views/personal_view.php
+++ application/modules/admin_personal_data/views/personal_view.php
@@ -7,13 +7,18 @@
                         <i class="fa fa-align-justify"></i> Globalindo Intimates - Personil Data (search)
                     </div>
                     <div class="card-body">
-                        <?php if ($this->session->flashdata('msg') != "") { echo $this->session->flashdata('msg'); } ?>
+                        <!-- </?php if ($this->session->flashdata('msg') != "") { echo $this->session->flashdata('msg'); } ?> -->
                         <div class="form-group">
-                        <label for="username" class="col-md-4 control-label"><span class="text-danger">*</span>Search By Name/Nik</label>
-                        <div class="col-md-8">
-                            <input type="text" name="nik" value="<?php echo $this->input->post('nik'); ?>" class="form-control" id="username" />
-                            <span class="text-danger"><?php echo form_error('nik');?></span>
-                        </div>
+                            <label for="username" class="col-md-4 control-label"><span class="text-danger">*</span>Search By Name/Nik</label>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <input type="text" name="nik" value="<?php echo $this->input->post('nik'); ?>" class="form-control" id="username" />
+                                    <span class="text-danger"><?php echo form_error('nik');?></span>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="#" class="btn btn-success">Search</a> 
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
